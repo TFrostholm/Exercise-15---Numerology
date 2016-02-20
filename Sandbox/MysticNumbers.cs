37ee623fe758a2083e2a3d55b5da22d74de7f0d9
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-20 15:26:12 +0100

    Rewrote method to use other methods as a better solution

--- Sandbox/MysticNumbers.cs
+++ Sandbox/MysticNumbers.cs
@@ -30,5 +30,32 @@ namespace Sandbox
 
             return result;
         }
+
+        public int TwoNumbers(int a, int b)
+        {
+            int result;
+
+            result = b > a ? b : a;
+            return result;
+        }
+
+        public int FourNumbers(int a, int b, int c, int d)
+        {
+            int x = TwoNumbers(a, b);
+            int y = TwoNumbers(c, d);
+            int result = TwoNumbers(x, y);
+
+            return result;
+        }
+
+        public int ThreeNumbersRewritten(int a, int b, int c)
+        {
+            int x = TwoNumbers(a, b);
+            int result = TwoNumbers(x, c);
+
+            return result;
+        }
+
+
     }
 }
